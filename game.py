7ff430b7be4b9939081a576fa2ce165a29af7125
Justin-Taliaferro <89564502+Justin-Taliaferro@users.noreply.github.com>
2021-09-21 12:16:15 -0400

    Update game.py

--- game.py
+++ game.py
@@ -11,4 +11,22 @@ options = ["Rock","Paper","Scissors"]
 computer_choice = random.choice(options)
 print("Computer Chose:")
 print(computer_choice)
+if computer_choice == user_choice:
+    print("You chose the same option, please pick again")
+elif user_choice == "rock":
+    if computer_choice == "scissors":
+        print ("Rock smashes scissors, congrats you won")
+    else:
+        print ("Paper covers rock, sorry you lost")
+elif user_choice == "scissors":
+    if computer_choice == "rock":
+        print ("Rock smashes scissors, sorry you lost")
+    else:
+        print ("Scissors cuts paper, congrats you won")
+elif user_choice == "paper":
+    if computer_choice == "rock":
+        print("Paper covers rock, congrats you won") 
+    else:
+        print("Scissors cuts paper, sorry you lost")
+        
 
